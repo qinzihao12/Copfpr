5d35cc5d9628e750cf6962a2ec8c296f783c6167
2580456922@qq.com <8885463+xx-zihao@user.noreply.gitee.com>
2022-07-11 16:57:26 +0800

    第一次

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<style>
+<style lang='scss'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primaryColor;
   margin-top: 60px;
 }
 </style>
